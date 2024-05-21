135850d4502c32218731ac6b621d63e21a19abf7
zahra <zahramalekiedu@gmail.com>
2024-05-21 22:51:05 +0330

    change article,time,report

--- model/feature_extraction/article_extractor.py
+++ model/feature_extraction/article_extractor.py
@@ -1,75 +1,53 @@
 import re
 from hazm import *
-from model.feature_extraction.law_extractor import law_extractor
 
 class article_extractor(object):
 
-    keywords = {
-    'ماده' , 'مواد'
-    }
+    articleKeywords = {'ماده', 'مواد', 'اصل', 'اصول'}
+    lawKeywords = ['قانون', 'قوانین', 'آیین نامه', 'آیین‌نامه', 'اساس نامه', 'اساس‌نامه']
 
-    def __init__(self , text):
+    def __init__(self, text):
+        self.result = [res for sentence in self.senTokenizer(text)
+                             for res in self.find_article(self.sentence_chunking(sentence), sentence)]
 
-        returnList = []
-        tokenized_sentences = self.senTokenizer(text)
+    def senTokenizer(self, text):
+        return sent_tokenize(text)
 
-        for sentence in tokenized_sentences:
-            chunking = self.sentence_chuncking(sentence)
-            findArticles = self.find_article(chunking)
-            extractor = law_extractor(text)
-            returnList.append(findArticles)
-            returnList.append(extractor.result)
-
-        self.result = returnList
-
-    def senTokenizer(self , text):
-
-        sentences = sent_tokenize(text)
-        return sentences
-        
-    def sentence_chuncking(self , sentence):
-
-        tagger = POSTagger(model='resource/pos_tagger.model')
-        chunker = Chunker(model='resource/chunker.model')
-
-        chunks = []
-        words = word_tokenize(sentence)
-        tagged_words = tagger.tag(words)
-
-
-        tree = chunker.parse(tagged_words)
-
-
-        for subtree in tree.subtrees():
-            if subtree.label() in ['NP', 'ADVP']:
-                chunk_text = tree2brackets(subtree)
-                chunks.append(chunk_text)
-
+    def sentence_chunking(self, sentence):
+        tagger = POSTagger(model='resource/hazm_model/pos_tagger.model')
+        chunker = Chunker(model='resource/hazm_model/chunker.model')
+        tagged_words = tagger.tag(word_tokenize(sentence))
+        chunks = [tree2brackets(subtree) for subtree in chunker.parse(tagged_words).subtrees() 
+                  if subtree.label() in ['NP', 'ADVP', 'ADJP']]
         return chunks
 
-    def find_article(self , chunks):
-        listOfNumber = []
-        i=0
+    def find_article(self, chunks, text):
+        article_indices = [i for i, chunk in enumerate(chunks) if any(kw in chunk for kw in self.articleKeywords)]
+        law_indices = [i for i, chunk in enumerate(chunks) if any(kw in chunk for kw in self.lawKeywords)]
+        return [self.find_all_in_one(chunks[i], text) if i in law_indices 
+                else self.find_it(chunks[i], self.articleKeywords, text) for i in article_indices]
 
-        while i < len(chunks):
+    def find_all_in_one(self, chunk, text):
+        words = chunk.split()
+        start = next(i for i, w in enumerate(words) if any(kw in w for kw in self.articleKeywords))
+        end = next(i for i, w in enumerate(words) if any(kw in w for kw in self.lawKeywords))
+        article = ' '.join(words[start:end])
+        return [article, *self.find_in_text(text, article)]
 
-            if any(key in chunks[i] for key in self.keywords):
-                listOfNumber = re.findall(r'\d+', chunks[i])
-                while chunks[i].find("قانون") == -1:
-                    i +=1
-                    if i < len(chunks):
-                        number = re.findall(r'\d+', chunks[i])
-                        if not number:
-                            break
-                        else:
-                            listOfNumber.append(number)
-                    else:
-                        break
-            i +=1
-        return listOfNumber
+    def find_it(self, chunk, keyChunk, text):
+        words = chunk.split()
+        start = next(i for i, w in enumerate(words) if any(kw in w for kw in keyChunk))
+        article = ' '.join(words[start:])
+        return [article, *self.find_in_text(text, article)]
 
+    def normalize_text(self, text):
+        return Normalizer().normalize(text)
 
-# text = "بر اساس ماده 1 و (10) قانون مجازات اسلامی"
-# extractor = article_extractor(text)
-# print(extractor.result)
+    def find_in_text(self, main_string, substring):
+        normalized_substring = self.normalize_text(substring)
+        start_index = main_string.find(normalized_substring)
+        return (start_index, start_index + len(normalized_substring) - 1) if start_index != -1 else (-1, -1)
 
+text = "عطف به نامه شماره ۹۱۹۲/۳۰۱۸۶ مورخ ۲۰/۲/۱۳۸۴ در اجرای اصل یکصد و بیست و سوم (۱۲۳) قانون اساسی جمهوری اسلامی ایران قانون مدیریت خدمات کشوری مصوب ۸/۷/۱۳۸۶ کمیسیون مشترک رسیدگی به لایحه مدیریت خدمات کشوری مجلس شورای اسلامی مطابق اصل هشتاد و پنجم (۸۵) قانون اساسی جمهوری اسلامی ایران که به مجلس شورای اسلامی تقدیم گردیده بود، پس از موافقت مجلس با اجرای آزمایشی آن به مدت پنج سال در جلسه علنی مورخ ۱۸/۱۱/۱۳۸۵ و تأیید شورای محترم نگهبان، به پیوست ارسال می گردد."
+extractor = article_extractor(text)
+print(extractor.result)
--- model/feature_extraction/time_extractor.py
+++ model/feature_extraction/time_extractor.py
@@ -1,79 +1,78 @@
 from hazm import *
 import re
 
-
-
 class time_extractor(object):
 
     months = {
-        ' فروردین' , 'اردیبهشت', 'خرداد',
+        'فروردین', 'اردیبهشت', 'خرداد',
         'تیر', 'مرداد', 'شهریور',
         ' مهر ', 'آبان', 'آذر',
         ' دی ', 'بهمن', 'اسفند'
     }
 
-    keywords = {
-        'ماه' ,'سال' , 'برج' , '/' , 'تاریخ'
-    }
-    def __init__(self , text):
+    def __init__(self, text):
         returnList = []
         tokenized_sentences = self.senTokenizer(text)
 
         for sentence in tokenized_sentences:
             dates = self.find_dates(sentence)
-            returnList.append(dates)
+            for date in dates:
+                if date:
+                    start, end = self.find_subtext_span(sentence, date)
+                    returnList.append((date, start, end))
 
         self.result = returnList
 
-    def senTokenizer(self , text):
-
+    def senTokenizer(self, text):
         sentences = sent_tokenize(text)
         return sentences
-    
-    def sentence_chuncking(self , sentence):
 
-        tagger = POSTagger(model='resource/pos_tagger.model')
-        chunker = Chunker(model='resource/chunker.model')
+    def sentence_chunking(self, sentence):
+        tagger = POSTagger(model='resource/hazm_model/pos_tagger.model')
+        chunker = Chunker(model='resource/hazm_model/chunker.model')
 
         chunks = []
         words = word_tokenize(sentence)
         tagged_words = tagger.tag(words)
-
-
         tree = chunker.parse(tagged_words)
 
-
         for subtree in tree.subtrees():
-            if subtree.label() in ['NP', 'ADVP']:
-                chunk_text = tree2brackets(subtree)
-                chunks.append(chunk_text)
+            # if subtree.label() in ['NP', 'ADVP']:
+            chunk_text = ' '.join(word for word, tag in subtree.leaves())
+            chunks.append(chunk_text)
 
         return chunks
-    
-    def find_dates(self , sentence):
-        shamsi_date_regex = r'\b(1[3-4][0-9]{2})[-/](0?[1-9]|1[0-2])[-/](0?[1-9]|[12][0-9]|3[01])\b'
 
-        dates = re.findall(shamsi_date_regex, sentence)
+    def find_dates(self, sentence):
+        
+        shamsi_date_regex_list ={
+            r'\b\d{4}\s*/\s*\d{1,2}\s*/\s*\d{1,2}\b',
+            r'\b\d{1,2}\s*/\s*\d{1,2}\s*/\s*\d{4}\b'
+        }
+        
+        listOfDates = []
+        for shamsi in shamsi_date_regex_list:
+            dates = re.findall(shamsi, sentence)
+            listOfDates.extend(dates)
 
-        if dates == []:
-            if any(month in sentence for month in self.months):
-                chunking = self.sentence_chuncking(sentence)
-                for i in range(len(chunking)):
-                    if any(m in chunking[i] for m in self.months):
-                        return chunking[i]            
-            elif any(keyword in sentence for keyword in self.keywords):
-                chunking = self.sentence_chuncking(sentence)
-                for i in range(len(chunking)):
-                    if any(k in chunking[i] for k in self.keywords):
-                         return chunking[i] 
-            else:
-                return []
-        else:
-            return dates
-                
+        if any(month in sentence for month in self.months):
+            chunks = self.sentence_chunking(sentence)
+            if len(chunks)>1:
+                for i in range(1,len(chunks)):
+                    
+                    if any(month in chunks[i] for month in self.months):
+                            # print(chunks[i])
+                            listOfDates.append(chunks[i])
+        return listOfDates
 
-                
-# text = "  در تاریخ  بیست و چهارم اردیبهشت ماه 1402 ،قانون مبارزه مدنی آمد"
+    def find_subtext_span(self, text, subtext):
+        current_index = 0
+        subtext_parts = subtext.split()
+        part_start = text.find(subtext_parts[0], current_index)
+        part_end = text.find(subtext_parts[-1], part_start) + len(subtext_parts[-1])
+        return part_start, part_end
+
+# Example usage
+# text =  "عطف به نامه شماره ۹۱۹۲/۳۰۱۸۶ مورخ ۲۰/۲/۱۳۸۴ در اجرای اصل یکصد و بیست و سوم (۱۲۳) قانون اساسی جمهوری اسلامی ایران قانون مدیریت خدمات کشوری مصوب ۸/۷/۱۳۸۶ کمیسیون مشترک رسیدگی به لایحه مدیریت خدمات کشوری مجلس شورای اسلامی مطابق اصل هشتاد و پنجم (۸۵) قانون اساسی جمهوری اسلامی ایران که به مجلس شورای اسلامی تقدیم گردیده بود، پس از موافقت مجلس با اجرای آزمایشی آن به مدت پنج سال در جلسه علنی مورخ 18/7/1385 و تأیید شورای محترم نگهبان در تاریخ بیست و چهارم مهر ماه، به پیوست ارسال می گردد." 
 # extractor = time_extractor(text)
 # print(extractor.result)
-    
