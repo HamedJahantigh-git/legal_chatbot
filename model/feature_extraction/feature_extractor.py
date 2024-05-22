355a167156debd1fef3e17190bf3bf9233651d8e
hamedjahantigh-git <n.hamedjahantigh@gmail.com>
2024-05-22 20:57:55 +0330

    inin feature_extractor class

--- model/feature_extraction/feature_extractor.py
+++ model/feature_extraction/feature_extractor.py
@@ -2,7 +2,9 @@
 
 from abc import ABC, abstractmethod
 from typing import List, Tuple
+from law_extractor import LawExtractor
 
+from hazm import *
 class Extractor(ABC):
     @abstractmethod
     def extract(input: str) -> list:
@@ -11,8 +13,26 @@ class Extractor(ABC):
 
 class FeatureExtrator():
     
-    def extract(input: str) -> dict:
-        pass
+    def __init__(
+        self, normalizer = Normalizer,
+        sentence_tokenizer = sent_tokenize,
+        word_tokenizer = word_tokenize, 
+        pos_tagger = POSTagger(model='../../resource/hazm_model/pos_tagger.model')
+        ) -> None:
+        self._normalizer = normalizer
+        self._sent_tokenizer = sentence_tokenizer
+        self._word_tokenizer = word_tokenizer
+        self._pos_tagger = pos_tagger
+        
+        self._law_extractor = LawExtractor(normalizer, 
+                                           word_tokenizer,
+                                           pos_tagger)
+        
+    def extract(self, input: str) -> List[dict]:
+        result = []
+        for sentence in self._sent_tokenizer(input):
+            law_phrase = self._law_extractor.extract(sentence)
+            
     
     def doce_type_detection():
         pass
