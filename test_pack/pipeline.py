2cc5d9e70c39d38f4fad591d50534ea30ef1a43d
miirzamiir <miirzamiir@gmail.com>
2024-05-22 13:54:56 +0330

    test pipeline update

--- test_pack/pipeline.py
+++ test_pack/pipeline.py
@@ -4,11 +4,14 @@ import json
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from model.feature_extraction import org_extractor
 from model.feature_extraction import law_extractor
+from model.feature_extraction import time_extractor
+from model.feature_extraction import article_extractor
+
 
 def testpipe():
 
-    f = open('test/zahra.json', 'r+', encoding='utf-8')
-    res = open('test/result.txt', 'w+', encoding='utf-8')
+    f = open('test_pack/zahra.json', 'r+', encoding='utf-8')
+    res = open('test_pack/result.txt', 'w+', encoding='utf-8')
     res.flush()
     testcases = json.load(f)
 
@@ -17,11 +20,11 @@ def testpipe():
 
     for i, testcase in enumerate(testcases):
         
-        # artext = article_extractor.article_extractor(testcase['input'])
-        # art = artext.result
+        artext = article_extractor.article_extractor(testcase['input'])
+        art = artext.result
 
-        # datext = time_extractor.time_extractor(testcase['input'])
-        # date = datext.result
+        datext = time_extractor.time_extractor(testcase['input'])
+        date = datext.result
         
         orgext = org_extractor.org_extractor(testcase['input'])
         org = orgext.find_org()
@@ -30,24 +33,27 @@ def testpipe():
         law = lawext.extract(testcase['input'])
 
         res.write(f'======== TEST {i+1:02d} ========\n')
-        res.write(f'input_text: {testcase["input"]}\n')
+        res.write(f'INPUT_TEXT: {testcase["input"]}\n')
 
-        # # statute reference / article
-        # if art == testcase['output']['Statute reference']:
-        #     res.write(f'PASSED ===> article: {art}')
-        #     s += 1
+        # statute reference / article
+        res.write('\nARTICLE:\n')
+
+        if art == testcase['output']['Statute reference']:
+            res.write(f'PASSED ===> article: {art}')
+            s += 1
         
-        # else:
-        #     res.write(f'FAILED ===> expected {testcase["output"]["Statute reference"]}, returned {art} \n')
+        else:
+            res.write(f'FAILED ===> expected {testcase["output"]["Statute reference"]}, returned {art} \n')
 
 
-        # # date / time
-        # if date == testcase['output']['date']:
-        #     res.write(f'PASSED ===> date: {date}')
-        #     s += 1
+        # date / time
+        res.write('\nDATE:\n')
+        if date == testcase['output']['Date']:
+            res.write(f'PASSED ===> date: {date}\n')
+            s += 1
         
-        # else:
-        #     res.write(f'FAILED ===> expected {testcase["output"]["date"]}, returned {date} \n')
+        else:
+            res.write(f'FAILED ===> expected {testcase["output"]["Date"]}, returned {date} \n')
 
 
         # defined terms / orgaization
@@ -69,9 +75,9 @@ def testpipe():
         else:
             res.write(f'FAILED ===> expected {testcase["output"]["law"]}, returned {law} \n')
 
-        res.write('\n')
+        res.write('\n\n')
 
     res.write('\n================================\nSTATS:\n')
-    res.write(f'TOTAL PASSED: {passed}/{total}\nSTATUTE REFERENCE: {s}/{total}\nDATE: {d}/{total}\nDEFINED TERMS: {o}/{total}\nLAW: {l}/{total}')
+    res.write(f'TOTAL PASSED: {passed}/{total}\nSTATUTE REFERENCE: {s}/{total}\ndate: {d}/{total}\nDEFINED TERMS: {o}/{total}\nLAW: {l}/{total}')
 
 testpipe()
