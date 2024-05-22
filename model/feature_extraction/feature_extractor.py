cb4f08f4f89e9b3a4c3b4f19994a1f6353ea55f7
hamedjahantigh-git <n.hamedjahantigh@gmail.com>
2024-05-22 20:35:20 +0330

    change imp of law with regex

--- model/feature_extraction/feature_extractor.py
+++ model/feature_extraction/feature_extractor.py
@@ -1,6 +1,7 @@
 # from ..dataset.dataset_types import LegalDataset
 
 from abc import ABC, abstractmethod
+from typing import List, Tuple
 
 class Extractor(ABC):
     @abstractmethod
@@ -15,4 +16,16 @@ class FeatureExtrator():
     
     def doce_type_detection():
         pass
+    
+    def _get_span(self, input: str, phrases: List[str]) -> List[Tuple[str, int, int]]:
+        index = 0
+        result = []
+        for phrase in phrases:
+            start_search_text = phrase[:phrase.find(" ")]
+            start_index = input.find(start_search_text, index)
+            end_search_text = phrase[phrase.rfind(" "):]
+            end_index = input.find(end_search_text, start_index)
+            index = end_index+len(end_search_text)
+            result.append((phrase, start_index,index))
+        return result
     
--- model/feature_extraction/law_extractor.py
+++ model/feature_extraction/law_extractor.py
@@ -1,4 +1,5 @@
 from typing import List, Tuple
+import re
 from hazm import *
 
 class LawExtractor():
@@ -6,7 +7,7 @@ class LawExtractor():
     def __init__(
         self, normalizer = Normalizer,
         tokenizer = word_tokenize, 
-        pos_tagger = POSTagger(model='resource/hazm_model/pos_tagger.model'),
+        pos_tagger = POSTagger(model='../../resource/hazm_model/pos_tagger.model'),
         keywords = [
         'قانون' , 'قوانین' , 'آیین نامه'  , 'آیین‌نامه' ,
         'اساس نامه' , 'اساس‌نامه']) -> None:
@@ -16,13 +17,14 @@ class LawExtractor():
         self._pos_tagger = pos_tagger
         self._keywords = keywords
         
-    def extract(self, input: str) -> List[Tuple[str, int, int]]:
+    def extract(self, input: str) -> List[str]:
         normal_text = self._normalizer.normalize(input)
         normal_text = normal_text.replace("،", " ،")
         tokens = self._tokenizer(normal_text)
         pos_tag = self._pos_tagger.tag(tokens)
-        phrases = self._pos_analysis(pos_tag)
-        return self._get_span(input, phrases)
+        first_phrases = self._pos_analysis(pos_tag)
+        phrase = self._regex_analysis(normal_text, first_phrases)
+        return phrase, first_phrases
     
     def _pos_analysis(self, pos_list: List) -> List:
         accept_tok = [index for index, tok in enumerate(pos_list)
@@ -32,23 +34,37 @@ class LawExtractor():
             index = i+1
             while ("EZ" in pos_list[index][1])or\
                 ("EZ" in pos_list[index-1][1]):
-                index += 1    
-            while (pos_list[index][0] == "،"  and (pos_list[index+2][0] in ["،", 'و']))or\
-                (pos_list[index][0] == "و"  and (pos_list[index-2][0] == "،"))or\
-                (pos_list[index-1][0] == "،" and (pos_list[index+1][0] in ["،", 'و'])or\
-                (pos_list[index-1][0] == "و" )):
-                index += 1
-            result.append(" ".join([word for word, tag in pos_list[i:index]]))
+                index += 1  
+            # while (pos_list[index][0] == "،"  and (pos_list[index+2][0] in ["،", 'و']))or\
+            #     (pos_list[index][0] == "و"  and (pos_list[index-2][0] == "،"))or\
+            #     (pos_list[index-1][0] == "،" and (pos_list[index+1][0] in ["،", 'و'])or\
+            #     (pos_list[index-1][0] == "و" )):
+            #     index += 1
+            result.append(" ".join([word for word,_ in pos_list[i:index]]))
         return result
     
-    def _get_span(self, input: str, phrases: List[str]) -> List[Tuple[str, int, int]]:
-        index = 0
+    def _regex_analysis(self, input, phrases: List[str])-> List[Tuple[str, int, int]]:
+        pattern = r"^(?:، \b\w+\b)+ و \b\w+\b"
         result = []
+        start = 0
         for phrase in phrases:
-            start_search_text = phrase[:phrase.find(" ")]
-            start_index = input.find(start_search_text, index)
-            end_search_text = phrase[phrase.rfind(" "):]
-            end_index = input.find(end_search_text, start_index)
-            index = end_index+len(end_search_text)
-            result.append((phrase, start_index,index))
+            end_index = self._get_end_span(input, phrase, start)
+            start = end_index
+            match = re.search(pattern, input[end_index+1:])
+            first_match = ""
+            if match:
+                first_match = match.group()
+            result.append(phrase+first_match)
         return result
+    
+    def _get_end_span(self, input: str, phrase: str, start: int) -> int:
+        index = start
+        start_search_text = phrase[:phrase.find(" ")]
+        start_index = input.find(start_search_text, index)
+        end_search_text = phrase[phrase.rfind(" "):]
+        end_index = input.find(end_search_text, start_index)
+        index = end_index+len(end_search_text)
+        return index
+        
+    
+    
