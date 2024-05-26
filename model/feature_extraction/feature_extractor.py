c05a2b6d5a5bca7a70fb326bd82c7154a7816b5c
hamedjahantigh-git <n.hamedjahantigh@gmail.com>
2024-05-26 16:55:16 +0330

    add feture extractor

--- model/feature_extraction/feature_extractor.py
+++ model/feature_extraction/feature_extractor.py
@@ -52,7 +52,66 @@ class FeatureExtractor:
 
         
         return self.result
+
+
+# from ..dataset.dataset_types import LegalDataset
+
+from abc import ABC, abstractmethod
+from typing import List, Tuple
+from law_extractor import LawExtractor
+from org_extractor import org_extractor
+
+from hazm import *
+class Extractor(ABC):
+    @abstractmethod
+    def extract(input: str) -> list:
+        pass
     
+
+class FeatureExtrator_H:
+    
+    def __init__(
+        self, normalizer = Normalizer,
+        sentence_tokenizer = sent_tokenize,
+        word_tokenizer = word_tokenize, 
+        pos_tagger = POSTagger(model='../../resource/hazm_model/pos_tagger.model')
+        ) -> None:
+        self._normalizer = normalizer
+        self._sent_tokenizer = sentence_tokenizer
+        self._word_tokenizer = word_tokenizer
+        self._pos_tagger = pos_tagger
+        
+        self._law_extractor = LawExtractor(normalizer, 
+                                           word_tokenizer,
+                                           pos_tagger)
+        self._org_extractor = org_extractor()
+        
+    def extract(self, input: str) -> List[dict]:
+        result = []
+        for sentence in self._sent_tokenizer(input):
+            law_phrase = self._law_extractor.extract(sentence)
+            
+            result.append({
+                "law": self._get_span(law_phrase),
+                
+            })
+    
+    def doce_type_detection():
+        pass
+    
+    def _get_span(self, input: str, phrases: List[str]) -> List[Tuple[str, int, int]]:
+        index = 0
+        result = []
+        for phrase in phrases:
+            start_search_text = phrase[:phrase.find(" ")]
+            start_index = input.find(start_search_text, index)
+            end_search_text = phrase[phrase.rfind(" "):]
+            end_index = input.find(end_search_text, start_index)
+            index = end_index+len(end_search_text)
+            result.append((phrase, "Span: "+f'[{start_index}, {index}]'))
+        return result
+    
+
 
 # text =  "هیئت وزیزان در جلسه ۱۸/۴/۹۹ به پیشنهاد ۳۸۶۵۴ مورخ ۱/۳/۹۷ وزارت تعاون، کار و رفاه اجتماعی به اتسناد اصل یک و سی وهشتم قانون اساسی جمهوری اسلامی ایران آیین نامه اجرایی بند خ ماده ۸۷ قانون برنامه ششم توسعه اقتصادی و اجتماعی و فرهنگی جمهوری اسلامی ایران -مصوب۱۳۹۵- را به شرح زیر تصویب کرد" 
 # ext = FeatureExtractor(text)
